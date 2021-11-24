47a156aa45417fc6d83c28c1fc9fc8a0e9e38b22
Mickael <mickaelfelizardo2008@gmail.com>
2021-11-23 21:34:30 -0300

    mover peças 1

--- index.js
+++ index.js
@@ -4,17 +4,45 @@ class Piece {
         this.lock = document.getElementById(lockId);
         this.face = document.createElement('div');
         this.face.className = `piece ${color}`;
+
+        this.reset();
     }
 
     reset() {
         this.lock.appendChild(this.face);
+        this.pos = -1;
         this.steps = 0;
         this.locked = true;
+    }
+
+    unlock() {
+        this.startCell.appendChild(this.face);
+        this.pos = this.startCell.id.match(/\d+/).at(0);
+        this.locked = false;
+    }
+
+    move(n) {
+        if (this.locked) {
+            if (n === 6) {
+                this.unlock();
+                return true;
+            } else return false;
+        }
+
+        tableCells[(this.pos + n) % 52].appendChild(this.face);
+        //check cells
+        this.pos = (this.pos + n) % 52;
+        //check cells
+        return true;
     }
 }
 
 class Player {
     constructor(name, color, pieceStartCellId) {
+        if (game.playerList.length >= 4) {
+            throw new Error('Número máximo de jogadores atingido');
+        }
+
         this.name = name;
         this.color = color;
         this.pieces = [
@@ -29,20 +57,19 @@ class Player {
 }
 
 class Game {
+    #instance = null;
+
     constructor() {
+        if (this.#instance) {
+            return this.#instance;
+        }
+        this.#instance = this;
         this.playerList = [];
     }
 
     addPlayer(player) {
-        if (this.playerList.length < 4) {
-            this.playerList.push(player);
-
-            for (let piece of player.pieces) {
-                piece.reset();
-            }
-
-            console.log('Jogador adicionado:', player);
-        }
+        this.playerList.push(player);
+        console.log('Jogador adicionado:', player);
     }
 
     start(button) {
@@ -71,3 +98,9 @@ function createPlayerFromCard(form, color, pieceStartCellId) {
 }
 
 const game = new Game();
+const tableCells = [];
+
+for (let i = 0; i < 52; i++) {
+    const cell = document.getElementById(`cell-${i}`);
+    tableCells.push(cell);
+}
